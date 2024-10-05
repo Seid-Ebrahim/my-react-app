f72a6d12508738abc451292d28f89f4ac658ac5c
Seid Ebrahim <Seidibrahim091@gmail.com>
2024-10-05 20:51:38 +0300

    Add deployment to the page

--- src/App.js
+++ src/App.js
@@ -18,7 +18,21 @@ function App() {
         >
           Learn React
         </a> */
-          <h1>Hello, world! New Branch</h1>
+          <>
+            /* <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+            // </a> */
+            <h1>Hello, world! New Branch</h1>
+            <h1> This is my first react app deployment</h1></>
         }
       </header>
     </div>
